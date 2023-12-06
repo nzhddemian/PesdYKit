abf132c50c9aad63e090cfccafdffd6edee53e75
Nezhdanov Demian <nzhd.demian@gmail.com>
2023-12-05 21:50:02 -0300

    iOS 12.0

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "PesdYKit",
     platforms: [
-      .macOS(.v12), .iOS(.v15)
+      .macOS(.v12), .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
